5ebb33cffa844ce511af08939b876a3f155fa01e
sinchanaudupa <sinchanaudupasv@gmail.com>
2026-07-27 21:52:31 +0530

    Time: 51 ms (81.03%), Space: 9.7 MB (75.86%) - LeetHub

---
+++ 2221-find-triangular-sum-of-an-array/2221-find-triangular-sum-of-an-array.c
@@ -0,0 +1,12 @@
+int triangularSum(int* nums, int numsSize) {
+    int n = numsSize;
+
+    while (n > 1) {
+        for (int i = 0; i < n - 1; i++) {
+            nums[i] = (nums[i] + nums[i + 1]) % 10;
+        }
+        n--;
+    }
+
+    return nums[0];
+}
