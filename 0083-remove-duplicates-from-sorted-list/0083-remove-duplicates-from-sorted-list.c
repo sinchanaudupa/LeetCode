327ce44bdc0d7ed468349fa5bdc38949568153cc
sinchanaudupa <sinchanaudupasv@gmail.com>
2026-08-21 22:02:12 +0530

    Time: 0 ms (100%), Space: 11.7 MB (50.66%) - LeetHub

---
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.c
@@ -0,0 +1,13 @@
+struct ListNode* deleteDuplicates(struct ListNode* head) {
+    struct ListNode* current = head;
+    
+    while (current && current->next) {
+        if (current->val == current->next->val) {
+            current->next = current->next->next;
+        } else {
+            current = current->next;
+        }
+    }
+    
+    return head;
+}
