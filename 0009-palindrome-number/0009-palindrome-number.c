5e06631aad9aace53fd1695dfd30e36b8e04127f
sinchanaudupa <sinchanaudupasv@gmail.com>
2026-09-23 22:11:32 +0530

    Time: 4 ms (24.42%), Space: 8.9 MB (62.51%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.c
@@ -0,0 +1,13 @@
+bool isPalindrome(int x) {
+    if (x < 0) return false;
+
+    int original = x;
+    long long rev = 0;
+
+    while (x > 0) {
+        rev = rev * 10 + x % 10;
+        x /= 10;
+    }
+
+    return original == rev;
+}
